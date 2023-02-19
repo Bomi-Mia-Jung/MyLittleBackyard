e06902e9ea20412569f557e20d97443ff9228f37
ACMiaJung <bojung24@amherst.edu>
2023-02-19 15:14:46 -0500

    add new table schema

---
+++ mylittlebackyard.database/dbo/Tables/Bag.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Bag] (
+    [Id]      INT           IDENTITY (1, 1) NOT NULL,
+    [BagName] NVARCHAR (50) NULL,
+    [Cost]    MONEY         NULL,
+    CONSTRAINT [PK_Bag] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ mylittlebackyard.database/dbo/Tables/ItemsInBag.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[ItemsInBag] (
+    [Id]     INT IDENTITY (1, 1) NOT NULL,
+    [ItemId] INT NOT NULL,
+    [BagId]  INT NULL,
+    CONSTRAINT [PK_ItemsInBag] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_ItemsInBag_Bag] FOREIGN KEY ([BagId]) REFERENCES [dbo].[Bag] ([Id]),
+    CONSTRAINT [FK_ItemsInBag_Item] FOREIGN KEY ([ItemId]) REFERENCES [dbo].[Item] ([Id])
+);
+
